b8e95db1e10e4cd5c274579cb9eaab6e48abd2f6
Ben Abbott <jaminscript@gmail.com>
2016-04-13 10:01:47 +1200

    Start validation code

---
+++ js/validation.js
@@ -0,0 +1,10 @@
+// Find the form
+var registrationForm = document.querySelector('#registration');
+
+// Wait for the user to submit the form
+registrationForm.onsubmit = function(event) {
+
+	// Stop the form from submitting
+	event.preventDefault();
+
+}
